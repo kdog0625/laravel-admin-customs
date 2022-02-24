e21f52c3479623cbf3bbe9cdeb30946df0307bd4
kdog0625 <kdoooog34@gmail.com>
2022-02-25 00:33:27 +0900

    quillの設定を追加

--- src/config/admin.php
+++ src/config/admin.php
@@ -413,6 +413,44 @@ return [
             'enable' => true,
             // Set the location of composer command
             'which-composer' => '/usr/local/bin/composer', // !! it's important !!
+        ],
+        'quill' => [
+            // If the value is set to false, this extension will be disabled
+            'enable' => true,
+            'config' => [
+                'modules' => [
+                    'syntax' => true,
+                    'toolbar' =>
+                        [
+                            ['size' => []],
+                            ['header' => []],
+                            'bold',
+                            'italic',
+                            'underline',
+                            'strike',
+                            ['script' => 'super'],
+                            ['script' => 'sub'],
+                            ['color' => []],
+                            ['background' => []],
+                            'blockquote',
+                            'code-block',
+                            ['list' => 'ordered'],
+                            ['list' => 'bullet'],
+                            ['indent' => '-1'],
+                            ['indent' => '+1'],
+                            'direction',
+                            ['align' => []],
+                            'link',
+                            'image',
+                            'video',
+                            'formula',
+                            'clean'
+                        ],
+                    "htmlEditButton" => ["syntax" => true, "debug" => true]
+                ],
+                'theme' => 'snow',
+                'height' => '200px',
+            ]
         ]
     ],
 ];
